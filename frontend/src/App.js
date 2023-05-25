51f2ee533b24ba262733b0040475d3f659592aa5
VinczeChris-FS <fs.cvincze@gmail.com>
2023-05-25 10:39:18 -0400

    Add CSS styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>Bill's Bookstore</h1>
+      <h2>Add a New Book</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minus quod
+        aperiam aliquid. Quas, inventore culpa doloremque, aspernatur, animi
+        quidem quae facilis libero iure alias omnis odit unde quos ab
+        accusantium.
+      </p>
+      <button>ADD BOOK</button>
+    </div>
+  );
 }
 
 export default App;
